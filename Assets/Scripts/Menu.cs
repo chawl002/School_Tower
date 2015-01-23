b92f3f0221380bef8c72a5f0c831d48f91d0dd0e
gliv001 <George.liv91@gmail.com>
2015-01-22 22:00:41 -0800

    Working Menu screen

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+	public Texture btnTexture;
+	
+	public float widthPercent = 0.3f;
+	public float heightPercent = 0.3f;
+	public float movex = 350f;
+	public float movey = 150f;
+
+	void OnGUI() {
+		if (!btnTexture) {
+			Debug.LogError("Please assign a texture on the inspector");
+			return;
+		}
+		Rect r = new Rect(movex,
+		                  movey,
+		                  Screen.width * widthPercent,
+		                  Screen.height * heightPercent); 
+
+		if (GUI.Button(r, btnTexture, GUIStyle.none)){
+			Debug.Log("Clicked the button with an image");
+			Application.LoadLevel("GameSelection");
+		}
+	}
+}
