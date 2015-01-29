8e96800f59ee0393dae5f94a9e29e80fcaf28ff3
gliv001 <George.liv91@gmail.com>
2015-01-29 14:56:52 -0800

    Added enemy prototype with movement
    Used a script that allowed the enemy to move from one waypoint to
    another.

---
+++ Assets/Scripts/EndReached.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndReached : MonoBehaviour {
+
+	void OnTriggerEvent2D(Collider2D co){
+		if (co.name == "Sphere")
+			Destroy (co.gameObject);
+	}
+}
---
+++ Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMove : MonoBehaviour {
+	public Transform[] waypoints;
+	int cur = 0;
+	
+	public float speed = 0.3f;
+	
+	void FixedUpdate () {
+		// Waypoint not reached yet? then move closer
+		if (transform.position != waypoints [cur].position) {
+			Vector2 p = Vector2.MoveTowards (transform.position,
+			                                waypoints [cur].position,
+			                                speed);
+			rigidbody2D.MovePosition (p);
+		} 
+		// Waypoint reached, select next one
+		else cur = (cur + 1) % waypoints.Length;
+	}
+}
