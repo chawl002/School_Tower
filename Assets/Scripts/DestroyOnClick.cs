0374ca198262443f8210d9c7a2610d8e3c872a29
jwestwinds <jwest009@ucr.edu>
2015-02-04 16:07:16 -0800

    DestroyOnClick
    Destroying a tower when clicking it is still in progress

---
+++ Assets/Scripts/DestroyOnClick.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyOnClick : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnMouseOver() {
+
+		Debug.Log ("YOU");
+
+		if (Input.GetMouseButtonDown (0)) {
+
+			Destroy(gameObject);
+
+				}
+
+	
+	}
+}
