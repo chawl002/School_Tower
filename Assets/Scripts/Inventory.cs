c55ccf29e4b28fa6aa08069aec3a38b6ae2d2c42
chawl002 <chawl002@ucr.edu>
2015-02-04 16:59:32 -0800

    Update on Inventory - included hover info menu

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -9,6 +9,8 @@ public class Inventory : MonoBehaviour {
 	public List<Item> slots = new List<Item> ();
 	private ItemDatabase database;
 	private bool showInventory = true;
+	private bool showToolTip;
+	private string tooltip;
 	// Use this for initialization
 	void Start () 
 	{
@@ -26,25 +28,33 @@ public class Inventory : MonoBehaviour {
 		AddItem (1);
 	}
 
+	//makes the inventory appear and disappear when the button i is pressed
 	void Update()
 	{
-		//key bindings to i
 		if(Input.GetButtonDown("Inventory"))
 		{
 			showInventory = !showInventory;
 		}
 	}
 
+	//draws the inventory if the showInventory bool is true.
+	//Creates the GUI box used in the hoverable item info
 	void OnGUI() 
 	{
+		tooltip = "";
 		GUI.skin = skin;
 		if(showInventory) 
 		{
 			DrawIventory();
 		}
+		if (showToolTip && showInventory)
+		{
+			GUI.Box (new Rect(Event.current.mousePosition.x, Event.current.mousePosition.y, 200, 200), tooltip);
+		}
 	}
 
 	//draws items in slots that exist in the slots/inventory lists, otherwise, draws an empty box for future items
+	//Creates infomation box over items in inventory
 	void DrawIventory()
 	{
 		int i = 0;
@@ -58,10 +68,26 @@ public class Inventory : MonoBehaviour {
 				if(slots[i].ItemName != null)
 				{
 					GUI.DrawTexture(slotRect, slots[i].ItemIcon);
+					if(slotRect.Contains (Event.current.mousePosition))
+					{
+						tooltip = CreateToolTip(slots[i]);
+						showToolTip = true;
+					}
+				}
+				if(tooltip == "")
+				{
+					showToolTip = false;
 				}
 				i++;
 			}
 		}
+	}
+
+	//Creation of the info in the hoverable info box
+	string CreateToolTip(Item item)
+	{
+		tooltip = item.ItemName;
+		return tooltip;
 	}
 
 	//Adds an item owned by the player to the inventory list.
@@ -83,5 +109,29 @@ public class Inventory : MonoBehaviour {
 
 		}
 	}
+	//if the item exists in the player's inventory, return true. else return false
+	bool FindItem(int id)
+	{
+		for (int i = 0; i < inventory.Count; i++) 
+		{
+			if (inventory[i].ItemID == id) 
+			{
+				return true;
+			}
+		}
+		return false;
+	}
 
+	//removes the first item that is found with the same ID number. Does not move other items up to fill empty spot currently
+	void RemoveItem(int id)
+	{
+		for (int i = 0; i < inventory.Count; i++) 
+		{
+			if (inventory[i].ItemID == id) 
+			{
+				inventory [i] = new Item ();
+				break;
+			}
+		}
+	}
 }
