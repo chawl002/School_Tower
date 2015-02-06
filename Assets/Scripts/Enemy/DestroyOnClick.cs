b515d81b1a9bfa246699c87159074733d242bb06
jwestwinds <jwest009@ucr.edu>
2015-02-05 16:28:49 -0800

    Timer
    Added timer. Game is won after 3 min, but no victory scene exists yet.

--- Assets/Scripts/Enemy/DestroyOnClick.cs
+++ Assets/Scripts/Enemy/DestroyOnClick.cs
@@ -2,7 +2,11 @@
 using System.Collections;
 
 public class DestroyOnClick : MonoBehaviour {
-	
+		
+	bool fclick = false;
+	bool sclick = false;
+	float temp = 0;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,12 +14,51 @@ public class DestroyOnClick : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if (fclick == true) {
+				temp += Time.deltaTime;
+				if(temp >= 0.5)
+				{
+					temp = 0;
+					fclick = false;
+				}
+
+				if(Input.GetKeyDown("w"))
+				{
+					fclick = false;
+					Debug.Log("up");
+				}
+
+				if(Input.GetKeyDown("a"))
+				{
+					fclick = false;
+					Debug.Log("left");
+				}
+
+				if(Input.GetKeyDown("s"))
+				{
+					fclick = false;
+					Debug.Log("down");
+				}
+
+				if(Input.GetKeyDown("d"))
+				{
+					fclick = false;
+					Debug.Log("right");
+				}
+
+			}
 
 	}
 
 	void OnMouseOver() {
 		//If unit is clicked, destroy it
-		if (Input.GetMouseButtonDown (0))
-			Destroy(gameObject);
+
+		if (Input.GetMouseButtonDown (0) && fclick == false) {
+						fclick = true;
+				} 
+		else if (Input.GetMouseButtonDown (0) && fclick == true) {
+						sclick = true;
+						Destroy(gameObject);
+				}
 	}
 }
--- Assets/Scripts/MoneyHandle/MoneyHandle.cs
+++ Assets/Scripts/MoneyHandle/MoneyHandle.cs
@@ -4,6 +4,11 @@ using System.Collections;
 public class MoneyHandle : MonoBehaviour {
 
 	public int mon;
+	public float timerf = 0;
+	public int timers = 0;
+	public int timerm = 0;
+	public bool victory = false;
+
 	public float temp = 0;
 	public int mtemp = 0;
 	public GUIStyle guicust;
@@ -15,21 +20,38 @@ public class MoneyHandle : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+		timerf += Time.deltaTime;
+
+		timers = (int)(timerf - (timerf % 1));
+
+		if (timers >= 60) {
+						timers = 0;
+						timerf = 0;
+						timerm += 1;
+				}
+
+		Debug.Log (timers);
+
 		temp += Time.deltaTime;
 		if ((temp-mtemp)%8 > 0) { //$1 given every 5 seconds
 						mtemp += 6;
 						mon += 100;
 				}
 
-		
+		if (timerm >= 3)
+						victory = true;
 
 	}
 
 	void OnGUI()
 	{
-		string extraZ = "";
+		string mextraZ = "";
 		if (mon % 100 < 10)
-						extraZ = "0";
-		GUI.Label (new Rect (-50, 0, Screen.width, Screen.height), "$ " + (mon/100).ToString() + "." + extraZ + (mon%100).ToString(), guicust);
+						mextraZ = "0";
+		string textraZ = "";
+		if (timers < 10)
+						textraZ = "0";
+		GUI.Label (new Rect (-50, 0, Screen.width, Screen.height), "\n$ " + (mon/100).ToString() + "." + mextraZ + (mon%100).ToString() + "\n\n\n\n" + timerm.ToString() + ":" + textraZ + timers.ToString(), guicust);
 	}
 }
