0385b7be464148afb3b1565a044d7a0fca66c291
chawl002 <chawl002@ucr.edu>
2015-02-05 18:26:52 -0800

    Fixed Menu and Level Select Button Placement
    will need additional fixing later

--- Assets/Scripts/Menu/LevelSelect.cs
+++ Assets/Scripts/Menu/LevelSelect.cs
@@ -18,14 +18,11 @@ public class LevelSelect : MonoBehaviour {
 			Debug.LogError("Please assign a texture on the inspector");
 			return;
 		}
-		Rect r = new Rect(220f,
-		                  150f,
-		                  Screen.width * widthPercent,
-		                  Screen.height * heightPercent); 
-		Rect r2 = new Rect(500f,
-		                  225f,
-		                  Screen.width * widthPercent,
-		                  Screen.height * heightPercent); 
+		//position of the elementary level choice
+
+		Rect r = new Rect(Screen.width / 2 - (Screen.width/8) - 190f, Screen.height/2 - 10f, Screen.width * widthPercent, Screen.height * heightPercent); 
+
+		Rect r2 = new Rect(Screen.width / 2 - (Screen.width/8) + 300f, Screen.height/2 + 90f, Screen.width * widthPercent, Screen.height * heightPercent); 
 
 		if (GUI.Button(r, btnEle, GUIStyle.none)){
 			Debug.Log("Clicked the button with an image");
--- Assets/Scripts/Menu/Menu.cs
+++ Assets/Scripts/Menu/Menu.cs
@@ -8,16 +8,13 @@ public class Menu : MonoBehaviour {
 	public float widthPercent = 0.3f;
 	public float heightPercent = 0.3f;
 
-
 	void OnGUI() {
 		if (!btnTexture) {
 			Debug.LogError("Please assign a texture on the inspector");
 			return;
 		}
-		Rect r = new Rect(350f,
-		                  180f,
-		                  Screen.width * widthPercent,
-		                  Screen.height * heightPercent); 
+		//Position of the Play button
+		Rect r = new Rect(Screen.width / 2 - (Screen.width/8), Screen.height/2, Screen.width * widthPercent, Screen.height * heightPercent); 
 
 		if (GUI.Button(r, btnTexture, GUIStyle.none)){
 			Debug.Log("Clicked the button with an image");
