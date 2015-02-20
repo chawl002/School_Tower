94dd8e07261c56ae05343a34e094673fe1f7f002
gliv001 <George.liv91@gmail.com>
2015-02-19 20:08:33 -0800

    Updated ProjPro script, towers are now able to shoot in four directions up, left, right, and down.

--- Assets/Scripts/Player/ProjPro.cs
+++ Assets/Scripts/Player/ProjPro.cs
@@ -2,7 +2,9 @@
 using System.Collections;
 
 public class ProjPro : MonoBehaviour {
-	
+	public float shoot_lr = 1;		// These two variables are used decide
+	public float shoot_ud = 0;		// which direction the towers will shoot
+
 	public float temp = 0;
 	//public GameObject proj;
 	public GameObject money;
@@ -32,8 +34,24 @@ public class ProjPro : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if(Input.GetKey ("up") || Input.GetKey("w") ){				// if the up arrow key or w is pressed then
+			shoot_lr = 0f;											// the next tower created will shoot up
+			shoot_ud = 1f;
+		}
+		else if(Input.GetKey ("down") || Input.GetKey("s") ){		// if the down arrow key or s is pressed then
+			shoot_lr = 0f;											// the next tower created will shoot down
+			shoot_ud = -1f;
+		}
+		else if(Input.GetKey ("left") || Input.GetKey("a") ){		// if the left arrow key or a is pressed then
+			shoot_lr = -1f;											// the next tower created will shoot left
+			shoot_ud = 0f;
+		}
+		else if(Input.GetKey ("right") || Input.GetKey("d") ){		// if the right arrow key or d is pressed then
+			shoot_lr = 1f;											// the next tower created will shoot right
+			shoot_ud = 0f;
+		}
 
-		transform.Translate(8*Time.deltaTime, 0, 0);
+		transform.Translate(shoot_lr*8*Time.deltaTime, shoot_ud*8*Time.deltaTime, 0);
 		//transform.position = Vector2.MoveTowards(transform.position, , 8 * Time.deltaTime);
 
 		temp += Time.deltaTime;
