45158d435e4d1c1b39eb40698d7e22476e6c03df
gliv001 <George.liv91@gmail.com>
2015-02-19 21:24:42 -0800

    Towers shoot in the desired direction specified (w,s,d,a) or arrow keys.

--- Assets/Scripts/Player/ProjPro.cs
+++ Assets/Scripts/Player/ProjPro.cs
@@ -2,8 +2,8 @@
 using System.Collections;
 
 public class ProjPro : MonoBehaviour {
-	public float shoot_lr = 1;		// These two variables are used decide
-	public float shoot_ud = 0;		// which direction the towers will shoot
+	public float lr = 0;		// These two variables are used decide
+	public float ud = 0;		// which direction the towers will shoot
 
 	public float temp = 0;
 	//public GameObject proj;
@@ -12,9 +12,6 @@ public class ProjPro : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-
-
-
 		money = GameObject.Find ("money");
 		if (money.GetComponent<MoneyHandle> ().mon - 15 < 0)
 			Destroy (gameObject);
@@ -30,28 +27,16 @@ public class ProjPro : MonoBehaviour {
 			a = 0;
 			b = 0;
 		}*/
+
+		lr = GetComponentInParent<Projectile>().shoot_lr;
+		ud = GetComponentInParent<Projectile>().shoot_ud;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKey ("up") || Input.GetKey("w") ){				// if the up arrow key or w is pressed then
-			shoot_lr = 0f;											// the next tower created will shoot up
-			shoot_ud = 1f;
-		}
-		else if(Input.GetKey ("down") || Input.GetKey("s") ){		// if the down arrow key or s is pressed then
-			shoot_lr = 0f;											// the next tower created will shoot down
-			shoot_ud = -1f;
-		}
-		else if(Input.GetKey ("left") || Input.GetKey("a") ){		// if the left arrow key or a is pressed then
-			shoot_lr = -1f;											// the next tower created will shoot left
-			shoot_ud = 0f;
-		}
-		else if(Input.GetKey ("right") || Input.GetKey("d") ){		// if the right arrow key or d is pressed then
-			shoot_lr = 1f;											// the next tower created will shoot right
-			shoot_ud = 0f;
-		}
 
-		transform.Translate(shoot_lr*8*Time.deltaTime, shoot_ud*8*Time.deltaTime, 0);
+		transform.Translate(lr*8*Time.deltaTime, ud*8*Time.deltaTime, 0);
 		//transform.position = Vector2.MoveTowards(transform.position, , 8 * Time.deltaTime);
 
 		temp += Time.deltaTime;
--- Assets/Scripts/Player/Projectile.cs
+++ Assets/Scripts/Player/Projectile.cs
@@ -2,15 +2,33 @@
 using System.Collections;
 
 public class Projectile : MonoBehaviour {
+	public float shoot_lr = 1;		// These two variables are used decide
+	public float shoot_ud = 0;		// which direction the towers will shoot
 
 	public float temp = 0;
 	public GameObject our;
 	public GameObject proj;
+	public GameObject projClone;
 
 
 	// Use this for initialization
 	void Start () {
-
+		if(Input.GetKey ("up") || Input.GetKey("w") ){				// if the up arrow key or w is pressed then
+			shoot_lr = 0f;											// the next tower created will shoot up
+			shoot_ud = 1f;
+		}
+		else if(Input.GetKey ("down") || Input.GetKey("s") ){		// if the down arrow key or s is pressed then
+			shoot_lr = 0f;											// the next tower created will shoot down
+			shoot_ud = -1f;
+		}
+		else if(Input.GetKey ("left") || Input.GetKey("a") ){		// if the left arrow key or a is pressed then
+			shoot_lr = -1f;											// the next tower created will shoot left
+			shoot_ud = 0f;
+		}
+		else if(Input.GetKey ("right") || Input.GetKey("d") ){		// if the right arrow key or d is pressed then
+			shoot_lr = 1f;											// the next tower created will shoot right
+			shoot_ud = 0f;
+		}
 
 	}
 	
@@ -24,7 +42,8 @@ public class Projectile : MonoBehaviour {
 			Vector3 pos = transform.position;
 
 
-			/*GameObject shot = */GameObject.Instantiate(proj, pos, transform.rotation); //Instantiate(tragedy);
+			projClone = Instantiate(proj, pos, transform.rotation) as GameObject; //Instantiate(tragedy);
+			projClone.transform.parent = gameObject.transform;
 			//shot.GetComponent<IndexInList>().INDEX = 
 			//DestroyObject(this);
 			//if(transform.root.gameObject){
