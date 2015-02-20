8efd3e5fbe325b1000420fda81cd840efb9835b3
jwestwinds <jwest009@ucr.edu>
2015-02-19 19:55:19 -0800

    Began Item Selection Spwaning
    This

--- Assets/SpawnPaperboy.cs
+++ Assets/SpawnPaperboy.cs
@@ -7,6 +7,10 @@ public class SpawnPaperboy : MonoBehaviour {
 	bool sclick = false;
 	float temp = 0;
 	public bool poop = false;
+	public int ItemIndex;
+	public GameObject Inv; // = GameObject.Find("Intentory");
+	public GameObject Id; // = GameObject.Find("Item Database");
+
 
 	// Use this for initialization
 	void Start () {
@@ -15,6 +19,10 @@ public class SpawnPaperboy : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		Inv = GameObject.Find("Intentory");
+		Id = GameObject.Find("Item Database");
+		ItemIndex = Inv.GetComponent<Inventory> ().SPAWNTOWER;
+
 		if (fclick == true) {
 			temp += Time.deltaTime;
 			if(temp >= 1)
@@ -29,22 +37,24 @@ public class SpawnPaperboy : MonoBehaviour {
 
 	void OnMouseOver() {
 		//If unit is clicked, destroy it
-		
-		if (Input.GetMouseButtonDown (0) && fclick == false) {
-			fclick = true;
-		} 
-		else if (Input.GetMouseButtonDown (0) && fclick == true && sclick == true) {
-			fclick = false;
-			sclick = false;
-			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-			Vector3 pos = new Vector3(ray.origin.x, ray.origin.y, -0.2f);
-			Instantiate(Resources.Load("LINK 1"), pos, transform.rotation);
-			poop = true;
-		}
-		else if (Input.GetMouseButtonDown (0) && fclick == true) {
-			sclick = true;
-		}
+		if (ItemIndex != -1) {
+		
+						if (Input.GetMouseButtonDown (0) && fclick == false) {
+								fclick = true;
+						} else if (Input.GetMouseButtonDown (0) && fclick == true && sclick == true) {
+								fclick = false;
+								sclick = false;
+								Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+								Vector3 pos = new Vector3 (ray.origin.x, ray.origin.y, -0.2f);
+								Instantiate (Resources.Load ("Towers/" + Id.GetComponent<ItemDatabase>().items[ItemIndex].ItemName), pos, transform.rotation);
+				//Debug.Log (Id.GetComponent<ItemDatabase>().items[ItemIndex].ItemName);
+								poop = true;
+						} else if (Input.GetMouseButtonDown (0) && fclick == true) {
+								sclick = true;
+						}
+				}
 
 	}
 }
