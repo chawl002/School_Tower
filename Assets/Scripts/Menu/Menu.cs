39b211a340e6ecf547d3a50e7ce8531aefaf445c
chawl002 <chawl002@ucr.edu>
2015-02-25 15:29:07 -0800

    Added Instructions to Menu System

--- Assets/Scripts/Menu/Menu.cs
+++ Assets/Scripts/Menu/Menu.cs
@@ -6,5 +6,11 @@ public class Menu : MonoBehaviour {
 	public void GoPlay(){
 		Application.LoadLevel(1);
 		}
-	
+	public void GoInstruct(){
+		Application.LoadLevel (5);
+	}
+	public void Restart()
+	{
+		Application.LoadLevel (0);
+	}
 }
