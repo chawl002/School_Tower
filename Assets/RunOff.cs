60e3479cda42bc204714fd0775764e31c6267859
jwestwinds <jwest009@ucr.edu>
2015-03-05 22:00:15 -0800

    Sound Effects!
    Such Fun! Much Wow!

--- Assets/RunOff.cs
+++ Assets/RunOff.cs
@@ -4,13 +4,18 @@ using System.Collections;
 public class RunOff : MonoBehaviour {
 
 	public bool ranaway = false;
-	public AudioClip audity;
+	public AudioSource aud_feet;
+	public AudioSource aud_pain;
 	bool audioplay = false;
+	bool audiopain = false;
 
 	float temp = 0;
 
 	// Use this for initialization
 	void Start () {
+
+		aud_feet = (AudioSource)gameObject.AddComponent ("AudioSource");
+		aud_pain = (AudioSource)gameObject.AddComponent ("AudioSource");
 
 	}
 	
@@ -19,9 +24,27 @@ public class RunOff : MonoBehaviour {
 
 		if (GetComponent<EnemyHP> ().HP <= 0) {
 
-						if(audioplay == false && audity)
+			if(audioplay == false)
 			{
-				audio.PlayOneShot(audity);
+				AudioClip myAudioClip;
+				myAudioClip = (AudioClip)Resources.Load ("Sounds/running_feet_-Cam-942211296");
+
+				AudioClip myAudioClip2;
+				myAudioClip2 = (AudioClip)Resources.Load ("Sounds/Pain-SoundBible.com-1883168362");
+
+
+				aud_pain.clip = myAudioClip2;
+
+				aud_pain.pitch += 1;
+
+				aud_pain.Play();
+
+				aud_feet.clip = myAudioClip;
+
+				aud_feet.Play();
+
+
+
 				audioplay = true;
 			}
 						
--- Assets/SoccerMom.cs
+++ Assets/SoccerMom.cs
@@ -4,11 +4,14 @@ using System.Collections;
 public class SoccerMom : MonoBehaviour {
 	
 	public bool is_mad = false;
+	public AudioSource aud_pain;
+	bool audioplay = false;
 	
 	// Use this for initialization
 	void Start () {
 		
 		GetComponent<EnemyMove> ().damageval = 0.0f;
+		aud_pain = (AudioSource)gameObject.AddComponent ("AudioSource");
 		
 	}
 	
@@ -20,6 +23,17 @@ public class SoccerMom : MonoBehaviour {
 	void OnTriggerEnter2D(Collider2D other)
 	{
 		if (other.tag == "attack" && other.name != "GUM(Clone)" && GetComponent<EnemyMove>().speed != 0f) {
+
+			if(audioplay == false)
+			{
+				AudioClip myAudioClip;
+				myAudioClip = (AudioClip)Resources.Load ("Sounds/Psycho Scream-SoundBible.com-1441943673");
+
+				aud_pain.clip = myAudioClip;
+
+				aud_pain.Play();
+			}
+
 			is_mad = true;
 			GetComponent<EnemyMove>().damageval = 0.25f;
 			GetComponent<EnemyMove>().speed = 0.3f;
