bc033c6d8e4cb1867dcbc9da1e5c313443927af0
jwestwinds <jwest009@ucr.edu>
2015-03-05 20:11:40 -0800

    Created Crammer Enemy
    Gains speed as he walks

---
+++ Assets/CrammerSpeed.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrammerSpeed : MonoBehaviour {
+	
+
+	// Use this for initialization
+	void Start () {
+
+		GetComponent<EnemyMove> ().speed -= 0.01f;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		GetComponent<EnemyMove> ().speed += 0.00005f;
+	
+	}
+}
