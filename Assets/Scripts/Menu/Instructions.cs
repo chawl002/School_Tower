f8ded6e981c09e4b384481a7afc0c5c8488e8244
gliv001 <George.liv91@gmail.com>
2015-03-10 20:32:56 -0700

    Created instruction template for multiple pages

---
+++ Assets/Scripts/Menu/Instructions.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Instructions	 : MonoBehaviour {
+	
+	public void GoInstruct1(){
+		Application.LoadLevel ("Instructions");
+	}
+	public void GoInstruct2(){
+		Application.LoadLevel ("Instructions2");
+	}
+	public void GoInstruct3(){
+		Application.LoadLevel ("Instructions3");
+	}
+	public void GoInstruct4(){
+		Application.LoadLevel ("Instructions4");
+	}
+	public void GoBack()
+	{
+		Application.LoadLevel ("OpeningTitle");
+	}
+}
