94402219449a14aacd9ffe65692b00ea7b3f5be1
jwestwinds <jwest009@ucr.edu>
2015-03-10 20:18:53 -0700

    Fixed Prefab

---
+++ Assets/KeepTrack.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeepTrack : MonoBehaviour {
+
+	int Gum, Pap, Bas, Dog, Pla, Can, Tac, Eff;
+
+	// Use this for initialization
+	void Start () {
+		Gum = 0;
+		Pap = 0;
+		Bas = 0;
+		Dog = 0;
+		Pla = 0;
+		Can = 0;
+		Tac = 0;
+		Eff = 0;
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
