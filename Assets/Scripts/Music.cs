e200b513c497f783bd0abb727fcb88c1445c4242
chawl002 <chawl002@ucr.edu>
2015-03-10 20:22:31 -0700

    Readded Music Script

---
+++ Assets/Scripts/Music.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class Music : MonoBehaviour {
+
+	public static Music instance = null;
+
+	void Start() 
+	{
+		audio.loop = true;
+		audio.rolloffMode = AudioRolloffMode.Custom;
+		if (!instance.audio.playOnAwake)
+			audio.Play ();
+
+	}
+
+	void Awake()
+	{
+		if (instance != null && instance != this)
+		{
+			Destroy(this.gameObject);
+			return;
+		}
+		else
+		{
+			instance = this;
+		}
+		DontDestroyOnLoad(this.gameObject);
+	}
+}
+
