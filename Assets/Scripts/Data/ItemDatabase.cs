1e3ba4b70adaff415bddbbfaf1ba7f4091d92db3
chawl002 <chawl002@ucr.edu>
2015-03-12 17:40:29 -0700

    Dodgeball prefab/images updated
    @jwestwinds to fix collider

--- Assets/Scripts/Data/ItemDatabase.cs
+++ Assets/Scripts/Data/ItemDatabase.cs
@@ -9,7 +9,7 @@ public class ItemDatabase : MonoBehaviour {
 		items.Add (new Item ("BubbleGum", 0, "Slows down enemies \n when they step on this.", 0, 0, 2.2, Item.ItemType.Stat, 25, "BubbleGumGirl"));
 		items.Add (new Item ("Paperwad", 1, "Deals 1 damage \n point towards enemies", 1, 8, 0, Item.ItemType.Weapon, 15, "PaperwadBoy"));
 		items.Add (new Item ("Baseball", 2, "Deals 1 damage \n point towards enemies. Can hit multiple enemies", 1, 8, 0, Item.ItemType.Weapon, 40, "BaseballBoy"));
-		items.Add (new Item ("dodgeballer", 3, "Deals 1 damage \n point towards nearby enemies. Can hit multiple enemies", 1, 8, 0, Item.ItemType.Weapon, 35, "DodgeballGirl"));
+		items.Add (new Item ("Dodgeball", 3, "Deals 1 damage \n point towards nearby enemies. Can hit multiple enemies", 1, 8, 0, Item.ItemType.Weapon, 35, "DodgeballGirl"));
 		items.Add (new Item ("paperplane", 4, "Deals 2 damage \n point towards nearby enemies.", 2, 7, 0, Item.ItemType.Weapon, 50, "PaperplaneGirl"));
 		items.Add (new Item ("candy", 5, "Slows an enemy down.", 0, 8, 0, Item.ItemType.Stat, 20, "Candykid"));
 		items.Add (new Item ("Tack", 6, "Hits an enemy with 1 point of damage. Slows enemy a bit.", 0, 8, 0, Item.ItemType.Stat, 40, "Tackkid"));
