2fbd521f388ce10ee2dec3de7ff65205dfdfda7c
gliv001 <George.liv91@gmail.com>
2015-03-12 20:49:55 -0700

    Message is displayed when bell event happens

--- Assets/Scripts/Player/BellReady_1.cs
+++ Assets/Scripts/Player/BellReady_1.cs
@@ -14,18 +14,20 @@ public class BellReady_1 : MonoBehaviour
 	public bool clicked = false;
 	public int rate = 2;
 	float temp = 0;
+	float msg_tmp = 5.0f;
 	bool fadeAudio = false;
 	public GameObject money;
-
+	public string eventMessage;
+	
 	//used to instanciate random events when bell is clicked
 	public int num_events = 4; //max size of EventDatabase list (calling item from 
 	private int rand_event; //event value number in list
 	private bool bell_event = true; //controls number of times rand function is called
 	public GameObject found_object;
-
+	
 	// Use this for initialization
 	void Start () {
-
+		
 		Rot.x = 0;
 		Rot.y = 0;
 		Rot.z = 1;
@@ -35,13 +37,13 @@ public class BellReady_1 : MonoBehaviour
 	void Update () 
 	{
 		ourColor = renderer.material.GetColor("_Color");
-
+		
 		if (clicked) 
 		{
 			//play the bell ring noise
 			if(!audio.isPlaying)
 			{audio.Play();}
-
+			
 			//pick a random event to execute once
 			if(bell_event)
 			{	
@@ -52,24 +54,29 @@ public class BellReady_1 : MonoBehaviour
 				if(rand_event < 25) //Player finds 5 dollar bill from bell event
 				{
 					Debug.Log("Player gains 500 money from bell");
-					money.GetComponent<MoneyHandle>().mon += 100;
+					money.GetComponent<MoneyHandle>().mon += 500;
+					eventMessage = "Player finds 5\n dollar bill";
 				}
 				else if(rand_event >= 25 && rand_event < 50) //Player finds 10 dollar bill from bell event
 				{
 					Debug.Log("Player gains 1000 money from bell");
-					money.GetComponent<MoneyHandle>().mon += 500;
+					money.GetComponent<MoneyHandle>().mon += 1000;
+					eventMessage = "Player finds 10\n dollar bill";
 				}
 				else if(rand_event >= 50 && rand_event < 75){//Player's towers are destroyed
 					Debug.Log("Player towers destroyed");
 					DestroyGameObjectsWithTag("tower");
 					found_object = GameObject.FindWithTag ("tower");
+					eventMessage = "Player's friends \nleft for class";
 				}
 				else if(rand_event >= 75){//Enemies are destroyed
-					Debug.Log("Enemies are destroyed");
+					Debug.Log("Enemies are \ndestroyed");
 					DestroyGameObjectsWithTag("enemy");
-				} 
+					eventMessage = "Enemies left for class";
+				}
+				msg_tmp = 0f; //displays message begins
 			}
-
+			
 			//updates time variable
 			temp+= Time.deltaTime;
 			//if the noise has played for 2 seconds, shut down.
@@ -77,23 +84,23 @@ public class BellReady_1 : MonoBehaviour
 			{
 				clicked = false;
 				GetComponent<BellFadeIn>().ready = false;
-
+				
 				color.r = ourColor.r;
 				color.g = ourColor.g;
 				color.b = ourColor.b;
 				color.a = 0.6f;
 				renderer.material.SetColor ("_Color", color);
-
+				
 				if (transform.rotation.z != 0)
 				{transform.rotation = Quaternion.identity;}
 				
 				temp = 0;
 				bell_event = true;
 				fadeAudio = true;
-
+				
 			}
 		}
-
+		
 		if (fadeAudio) 
 		{
 			audio.volume -= 0.1f;
@@ -103,33 +110,41 @@ public class BellReady_1 : MonoBehaviour
 				audio.volume = 1.0f;
 				fadeAudio = false;
 			}
-
+			
 		}
-
+		
 		if (ourColor.a == 1) 
 		{
 			if (rotfin == true) {
-
-					roti = -1 * roti;
-					rotdir = -1 * rotdir;
-					rotfin = false;
+				
+				roti = -1 * roti;
+				rotdir = -1 * rotdir;
+				rotfin = false;
 			}
-
+			
 			if(clicked == true)
 			{
 				rate = 8;
 			}
-
+			
 			transform.Rotate (Rot * rotdir*rate);
 			rate = 1;
-
+			
 			if (GetComponent<Transform> ().rotation.z * GetComponent<Transform> ().rotation.z >= roti * roti)
 			{rotfin = true;}
-
+			
 			mover = false;
 		}
 	}
-
+	
+	void OnGUI(){
+		if(msg_tmp < 4.0f){ 
+			GUI.Box (new Rect(Input.mousePosition.x,200-Input.mousePosition.y, 200, 50), eventMessage);
+			msg_tmp += Time.deltaTime;
+		}
+	}
+	
+	
 	//check to see if user has clicked the bell
 	void OnMouseOver()
 	{
@@ -143,7 +158,7 @@ public class BellReady_1 : MonoBehaviour
 			clicked = true;
 		} 
 	}
-
+	
 	public static void DestroyGameObjectsWithTag(string tag)
 	{
 		GameObject[] gameObjects = GameObject.FindGameObjectsWithTag(tag);
